b427f6ce02413b20e479244f942cf15993806d0e
darik aflah <darikaflah@gmail.com>
2023-03-09 09:35:25 +0700

    OOP DARIK

---
+++ AppInventorySql/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace AppInventorySql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Kode = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.ADD = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Nama = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Stok = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Harga = new System.Windows.Forms.TextBox();
+            this.EDIT = new System.Windows.Forms.Button();
+            this.DELETE = new System.Windows.Forms.Button();
+            this.Cari = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Imagetext = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kode Barang";
+            // 
+            // Kode
+            // 
+            this.Kode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Kode.Location = new System.Drawing.Point(12, 161);
+            this.Kode.Name = "Kode";
+            this.Kode.Size = new System.Drawing.Size(218, 26);
+            this.Kode.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Location = new System.Drawing.Point(247, 44);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 100;
+            this.dataGridView1.Size = new System.Drawing.Size(569, 342);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "Kode_Barang";
+            this.Column1.HeaderText = "Kode_Barang";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 75;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "Nama_Barang";
+            this.Column2.HeaderText = "Nama_Barang";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 90;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "Stok_Barang";
+            this.Column3.HeaderText = "Stok_Barang";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 75;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "Satuan_Barang";
+            this.Column4.HeaderText = "Satuan_Barang";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 85;
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "Harga_Barang";
+            this.Column5.HeaderText = "Harga_Barang";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "Gambar_Barang";
+            this.Column6.FillWeight = 200F;
+            this.Column6.HeaderText = "Gambar_Barang";
+            this.Column6.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Stretch;
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // ADD
+            // 
+            this.ADD.Location = new System.Drawing.Point(247, 396);
+            this.ADD.Name = "ADD";
+            this.ADD.Size = new System.Drawing.Size(182, 42);
+            this.ADD.TabIndex = 3;
+            this.ADD.Text = "ADD";
+            this.ADD.UseVisualStyleBackColor = true;
+            this.ADD.Click += new System.EventHandler(this.ADD_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nama Barang";
+            // 
+            // Nama
+            // 
+            this.Nama.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nama.Location = new System.Drawing.Point(12, 224);
+            this.Nama.Name = "Nama";
+            this.Nama.Size = new System.Drawing.Size(218, 26);
+            this.Nama.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Stok Barang";
+            // 
+            // Stok
+            // 
+            this.Stok.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Stok.Location = new System.Drawing.Point(12, 285);
+            this.Stok.Name = "Stok";
+            this.Stok.Size = new System.Drawing.Size(218, 26);
+            this.Stok.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 326);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Satuan Barang";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 389);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Harga Barang";
+            // 
+            // Harga
+            // 
+            this.Harga.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Harga.Location = new System.Drawing.Point(16, 412);
+            this.Harga.Name = "Harga";
+            this.Harga.Size = new System.Drawing.Size(214, 26);
+            this.Harga.TabIndex = 11;
+            // 
+            // EDIT
+            // 
+            this.EDIT.Location = new System.Drawing.Point(435, 396);
+            this.EDIT.Name = "EDIT";
+            this.EDIT.Size = new System.Drawing.Size(185, 42);
+            this.EDIT.TabIndex = 12;
+            this.EDIT.Text = "EDIT";
+            this.EDIT.UseVisualStyleBackColor = true;
+            this.EDIT.Click += new System.EventHandler(this.EDIT_Click);
+            // 
+            // DELETE
+            // 
+            this.DELETE.Location = new System.Drawing.Point(626, 396);
+            this.DELETE.Name = "DELETE";
+            this.DELETE.Size = new System.Drawing.Size(190, 42);
+            this.DELETE.TabIndex = 13;
+            this.DELETE.Text = "DELETE";
+            this.DELETE.UseVisualStyleBackColor = true;
+            this.DELETE.Click += new System.EventHandler(this.DELETE_Click);
+            // 
+            // Cari
+            // 
+            this.Cari.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cari.Location = new System.Drawing.Point(361, 12);
+            this.Cari.Name = "Cari";
+            this.Cari.Size = new System.Drawing.Size(455, 26);
+            this.Cari.TabIndex = 14;
+            this.Cari.TextChanged += new System.EventHandler(this.Cari_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(250, 14);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 24);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Cari Kode :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 7);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(123, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Gambar Barang";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(208, 103);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Imagetext
+            // 
+            this.Imagetext.Location = new System.Drawing.Point(12, 33);
+            this.Imagetext.Name = "Imagetext";
+            this.Imagetext.Size = new System.Drawing.Size(208, 20);
+            this.Imagetext.TabIndex = 18;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "PCS",
+            "KG",
+            "SACHET"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 349);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(218, 28);
+            this.comboBox1.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Cari);
+            this.Controls.Add(this.DELETE);
+            this.Controls.Add(this.EDIT);
+            this.Controls.Add(this.Harga);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Stok);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Nama);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ADD);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Kode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Imagetext);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CRUD OOP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Kode;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button ADD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Nama;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Stok;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Harga;
+        private System.Windows.Forms.Button EDIT;
+        private System.Windows.Forms.Button DELETE;
+        private System.Windows.Forms.TextBox Cari;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox Imagetext;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewImageColumn Column6;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ AppInventorySql/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
+using MySqlX.XDevAPI.Relational;
+
+namespace AppInventorySql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ADD_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.Kode.Text))
+            {
+                MessageBox.Show("Kode Barang harus diisi terlebih dahulu.");
+                return;
+            }
+            else
+            {
+                string connection = "server=localhost;user id=root;password=;database=oopcrud";
+                MySqlConnection conn = new MySqlConnection(connection);
+
+                string query = "INSERT INTO barang(kode_barang,nama_barang,stok_barang,satuan_barang,harga_barang,image)VALUES(@kode, @nama, @stok, @satuan, @harga, @image)";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@kode", this.Kode.Text);
+                cmd.Parameters.AddWithValue("@nama", this.Nama.Text);
+                cmd.Parameters.AddWithValue("@stok", this.Stok.Text);
+                cmd.Parameters.AddWithValue("@satuan", this.comboBox1.SelectedItem.ToString());
+                cmd.Parameters.AddWithValue("@harga", this.Harga.Text);
+                cmd.Parameters.AddWithValue("@image", Path.GetFileName(pictureBox1.ImageLocation));
+
+                conn.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                // Copy image to destination folder
+                File.Copy(Imagetext.Text, Application.StartupPath + @"\Image\" + Path.GetFileName(pictureBox1.ImageLocation), true);
+                conn.Close();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Data berhasil disimpan.");
+
+                    // Refresh data grid view
+                    string selectQuery = "SELECT * FROM barang";
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(selectQuery, conn);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    dt.Columns.Add("Gambar_Barang", Type.GetType("System.Byte[]"));
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        row["Gambar_Barang"] = File.ReadAllBytes(Application.StartupPath + @"\Image\" + Path.GetFileName(row["image"].ToString()));
+                    }
+                    dataGridView1.DataSource = dt;
+
+                    // Clear input fields
+                    this.Kode.Text = "";
+                    this.Nama.Text = "";
+                    this.Stok.Text = "";
+                    this.comboBox1.SelectedIndex = -1;
+                    this.Harga.Text = "";
+                    this.pictureBox1.Image = null;
+
+                }
+            }
+        }
+
+        private void EDIT_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.Kode.Text))
+            {
+                MessageBox.Show("Kode Barang harus diisi terlebih dahulu.");
+                return;
+            }
+            string connection = "server=localhost;user id=root;password=;database=oopcrud";
+            string query = "UPDATE barang SET nama_barang=@nama, harga_barang=@harga, stok_barang=@stok, satuan_barang=@satuan WHERE kode_barang=@kode";
+            MySqlConnection conn = new MySqlConnection(connection);
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@nama", this.Nama.Text);
+            cmd.Parameters.AddWithValue("@harga", this.Harga.Text);
+            cmd.Parameters.AddWithValue("@stok", this.Stok.Text);
+            cmd.Parameters.AddWithValue("@satuan", this.comboBox1.Text);
+            cmd.Parameters.AddWithValue("@kode", this.Kode.Text);
+            conn.Open();
+            MySqlDataReader reader = cmd.ExecuteReader();
+            MessageBox.Show("Data berhasil diupdate.");
+            conn.Close();
+
+            // Refresh data grid view
+            string selectQuery = "SELECT * FROM barang";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(selectQuery, conn);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dt.Columns.Add("Gambar_Barang", Type.GetType("System.Byte[]"));
+            foreach (DataRow row in dt.Rows)
+            {
+                row["Gambar_Barang"] = File.ReadAllBytes(Application.StartupPath + @"\Image\" + Path.GetFileName(row["image"].ToString()));
+            }
+            dataGridView1.DataSource = dt;
+            // Clear input fields
+            this.Kode.Text = "";
+            this.Nama.Text = "";
+            this.Stok.Text = "";
+            this.comboBox1.SelectedIndex = -1;
+            this.Harga.Text = "";
+            this.pictureBox1.Image = null;
+
+        }
+
+        private void DELETE_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.Kode.Text))
+            {
+                MessageBox.Show("Kode barang tidak boleh kosong.");
+                return;
+            }
+            else
+            {
+                string connection = "server=localhost;user id=root;password=;database=oopcrud";
+                string query = "DELETE FROM barang WHERE kode_barang='" + this.Kode.Text + "'";
+                string imagePath = Application.StartupPath + @"\Image\" + Path.GetFileName(pictureBox1.ImageLocation);
+
+                if (File.Exists(imagePath))
+                {
+                    File.Delete(imagePath);
+                }
+
+                MySqlConnection conn = new MySqlConnection(connection);
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataReader dr;
+
+                conn.Open();
+                dr = cmd.ExecuteReader();
+                MessageBox.Show("Berhasil Dihapus");
+                conn.Close();
+
+                // Refresh data grid view
+                string selectQuery = "SELECT * FROM barang";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(selectQuery, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dt.Columns.Add("Gambar_Barang", Type.GetType("System.Byte[]"));
+                foreach (DataRow row in dt.Rows)
+                {
+                    row["Gambar_Barang"] = File.ReadAllBytes(Application.StartupPath + @"\Image\" + Path.GetFileName(row["image"].ToString()));
+                }
+                dataGridView1.DataSource = dt;
+
+                // Kosongkan semua kolom input
+                this.Kode.Text = "";
+                this.Nama.Text = "";
+                this.Stok.Text = "";
+                this.comboBox1.SelectedIndex = -1;
+                this.Harga.Text = "";
+                this.pictureBox1.Image = null;
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connection = "server=localhost;user id=root;password=;database=oopcrud";
+            string query = "SELECT * FROM barang";
+            MySqlConnection conn = new MySqlConnection(connection);
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            adapter.SelectCommand = cmd;
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dt.Columns.Add("Gambar_Barang", Type.GetType("System.Byte[]"));
+            foreach (DataRow row in dt.Rows)
+            {
+                row["Gambar_Barang"] = File.ReadAllBytes(Application.StartupPath + @"\Image\" + Path.GetFileName(row["image"].ToString()));
+            }
+            dataGridView1.DataSource = dt;
+
+            // Sembunyikan kolom image
+            dataGridView1.Columns["image"].Visible = false;
+
+
+        }
+
+        private void Cari_TextChanged(object sender, EventArgs e)
+        {
+            string connection = "server=localhost;user id=root;password=;database=oopcrud";
+            MySqlConnection con = new MySqlConnection(connection);
+            MySqlDataAdapter da;
+            DataTable dt;
+            con.Open();
+            da = new MySqlDataAdapter("SELECT * FROM barang WHERE kode_barang LIKE'" + this.Cari.Text + "%'", con);
+            dt = new DataTable();
+            da.Fill(dt);
+            dt.Columns.Add("Gambar_Barang", Type.GetType("System.Byte[]"));
+            foreach (DataRow row in dt.Rows)
+            {
+                row["Gambar_Barang"] = File.ReadAllBytes(Application.StartupPath + @"\Image\" + Path.GetFileName(row["image"].ToString()));
+            }
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfd = new OpenFileDialog();
+            openfd.Filter = "Image Files(*.jpg;*.jpeg;*.gif;) | *.jpg;*.jpeg;*.gif; ";
+            if (openfd.ShowDialog() == DialogResult.OK)
+            {
+                Imagetext.Text = openfd.FileName;
+                pictureBox1.Image = new Bitmap(openfd.FileName);
+                pictureBox1.ImageLocation = openfd.FileName;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+    }
+}
